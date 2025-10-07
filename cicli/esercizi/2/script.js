9d7192c83fb13d7c6c23bc494b30bd5618bcbc30
Erik Fakin <e.fakin3@gmail.com>
2025-10-07 13:10:18 +0200

    cicli esercizio 2

--- cicli/esercizi/2/script.js
+++ cicli/esercizi/2/script.js
@@ -1,7 +1,7 @@
 const numero = prompt("Inserisci un numero da 1 a infinito: ");
 
 for(let i=1; i < numero; i++){
-    if(i % 7 == 0){
+    if(i % 7 == 0 && i % 3 == 0){
         console.log(i)
     }
 }
